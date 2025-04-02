c55603465cd34e29db8f63d9e7ed53aa04cf1c93
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-04-02 04:22:22 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -16,7 +16,7 @@ haxelib install hxvlc 2.0.1 --skip-dependencies
 haxelib set lime 8.1.2
 haxelib set openfl 9.3.3
 haxelib install SScript
-haxelib install hxCodec 2.5.1
+haxelib git hxCodec https://github.com/MatthewFro191919/hxCodec-2.5.1-MacOS-and-Linux-Support.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
